807686688cf088bd70d7713d6860e73049fdc46b
aaswissa <aviran.swissa@gmail.com>
2022-01-21 14:55:43 +0200

    sample package changed to mongodb,should argparse remove pack and print diff - TOTAL COMAPRE

--- cpu_test.py
+++ cpu_test.py
@@ -5,28 +5,49 @@ import argparse
 
 def cpu_test():
 
-        parser = argparse.ArgumentParser(description='Time to get CPU per seconds')
+        parser = argparse.ArgumentParser(description='Get CPU usage per seconds')
+
         parser.add_argument('-t', '--time',
-                        type=int, help='python cpu_test -t 4 => 4 seconds of CPU' )
+                        type=int, default=4, help='Example: python cpu_test -t 4')
+
+        parser.add_argument('-p', '--package',
+                        type=int, help='Example: python -p mongodb')
+
+        parser.add_argument('-e', '--exec',
+                        type=int, help='Example: python -a mongod')
+
+        parser.add_argument('-k', '--kill',
+                        type=int, help='Example: python -k mongod')
+
+        parser.add_argument('-r', '--remove',
+                        type=int, help='Example: python -r mongodb')
 
         args = parser.parse_args()
+
         time = args.time
+        package = args.package
+        exec = args.exec
+        kill = args.kill
 
         cpu_before = psutil.cpu_percent(time)
 
-        subprocess.check_call("sudo apt -y install libreoffice", shell=True)
-        cpu_after = psutil.cpu_percent(time)
+        try:
+            subprocess.check_call(f"sudo apt -y install {package}", shell=True)
+        except subprocess.CalledProcessError:
+            
+            cpu_after = psutil.cpu_percent(time)
+            subprocess.check_call(f"{exec} &", shell=True)
+            cpu_open = psutil.cpu_percent(time)
 
-        subprocess.check_call("libreoffice &", shell=True)
-        cpu_open = psutil.cpu_percent(time)
-
-        subprocess.check_call("killall soffice.bin", shell=True)
-        subprocess.check_call("sudo apt-get -y remove libreoffice", shell=True)
+            subprocess.check_call(f"kill -SIGINT `pgrep {kill}`", shell=True)
+            subprocess.check_call("sudo apt-get -y remove {libreoffice}", shell=True)
 
         print("****************************")
         print(f"CPU of {time} sec. before install: {cpu_before}%")
         print(f"CPU of {time} sec. after install: {cpu_after}%")
         print(f"CPU of {time } sec. after open: {cpu_open}&")
+
+        print("*********TOTAL COMAPRE HERE*******************")
 
         return cpu_before,cpu_after,cpu_open   
 
